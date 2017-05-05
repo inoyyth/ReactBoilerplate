bf3193a7eacb0f738ce7ee53d6f36d0f00aab329
supriyadin inoy <supriyadin inoy>
2017-05-05 14:48:04 +0700

    master file

---
+++ server.js
@@ -0,0 +1,10 @@
+var express  = require('express');
+
+//Create Our app
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('express server is up on port 3000');
+});
