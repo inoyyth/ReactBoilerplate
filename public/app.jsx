4314662d981009946a32e730786bca184b4c7aa8
inoy yth <supri170845@gmail.com>
2017-05-08 15:08:05 +0700

    video011, component properties

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,26 @@
 var Greeter = React.createClass({
+  getDefaultProps: function () { //for set default properties(props)
+    return {
+      name: 'React',
+      message: 'default message'
+    }
+  },
   render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Hello React !</h1>
-        <p>This is from a component !</p>
+        <h1>Hello {name} !</h1>
+      <p>{message + '!!!'}</p>
       </div>
     );
   }
 });
 
+var firstName = 'Inoy';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message="Hai, this message from props!"/>,
   document.getElementById('app')
 );
