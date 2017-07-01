765421220175df9f7338e26daa3ababb214cc6fb
inoyyth <supri170845@gmail.com>
2017-07-01 16:56:28 +0700

    024 boilerplage project

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Greeter = require('Greeter');
-
-var firstName = 'Inoy YTH';
 
 ReactDOM.render(
-  <Greeter name={firstName} message="Hai, this message from props!"/>,
+  <h1>Boilerplate app!</h1>,
   document.getElementById('app')
 );
